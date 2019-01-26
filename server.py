fc00729d474726a343fb98eb57d462d95aa1b1f3
LucaJaich <jaichencol@gmail.com>
2019-01-26 05:04:20 -0300

    sentence splitted correctly

--- server.py
+++ server.py
@@ -31,18 +31,19 @@ def analyze_sentence(audio):
 	file = FileStorage(audio)
 	file.save("sentence/sentence.wav")
 
-	sound_file = AudioSegment.from_wav("a-z.wav")
+	sound_file = AudioSegment.from_file("sentence/sentence.wav")
 	audio_chunks = split_on_silence(sound_file, 
     # must be silent for at least half a second
     min_silence_len=100,
 
     # consider it silent if quieter than -16 dBFS
-    silence_thresh=-16
+    silence_thresh=-10
 	)
 
+	print("words: ", len(audio_chunks))
 	for i, chunk in enumerate(audio_chunks):
 
-	    out_file = ".//splitAudio//chunk{0}.wav".format(i)
+	    out_file = "sentence/chunk{0}.wav".format(i)
 	    print("exporting", out_file)
 	    chunk.export(out_file, format="wav")
 
