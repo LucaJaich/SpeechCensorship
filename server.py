b6f2b72bffa1e34fb722ca5f773af447bf362369
LucaJaich <jaichencol@gmail.com>
2019-01-26 18:05:41 -0300

    trying to make index audio worl

--- server.py
+++ server.py
@@ -1,13 +1,10 @@
 from flask import Flask, request
 from flask_cors import CORS
 import json
-import pygame
 from watson_developer_cloud import TextToSpeechV1, SpeechToTextV1
 from werkzeug.datastructures import FileStorage
 import os
-from pydub import AudioSegment
-from pydub.silence import split_on_silence
-
+from SimpleAudioIndexer import SimpleAudioIndexer as sai
 
 
 app = Flask(__name__)
@@ -26,26 +23,17 @@ def analyze_word(audio):
 	file.save("word.wav")
 
 def analyze_sentence(audio):
+	'''
 	os.system("rm sentence/*")
 		
 	file = FileStorage(audio)
 	file.save("sentence/sentence.wav")
-
-	sound_file = AudioSegment.from_file("sentence/sentence.wav")
-	audio_chunks = split_on_silence(sound_file, 
-    # must be silent for at least half a second
-    min_silence_len=100,
-
-    # consider it silent if quieter than -16 dBFS
-    silence_thresh=-10
-	)
-
-	print("words: ", len(audio_chunks))
-	for i, chunk in enumerate(audio_chunks):
-
-	    out_file = "sentence/chunk{0}.wav".format(i)
-	    print("exporting", out_file)
-	    chunk.export(out_file, format="wav")
+	'''
+	indexer = sai(mode="ibm", src_dir="sentence", username_ibm="b6bf9c08-1c6d-4f69-9aa7-f6795920f1c3", password_ibm="mqk7rw8X8isy")
+	#indexer.index_audio(basename="sentence.wav", model="es-ES_NarrowbandModel")
+	indexer.index_audio(basename="sentence.wav")
+	indexer.save_indexed_audio("{}/indexed_audio".format(indexer.src_dir))
+	print(indexer.get_timestamps())
 
 @app.route("/audio", methods=['POST'])
 def manage_request():
